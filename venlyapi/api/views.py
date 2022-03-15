8b94fedb35f878a988f629afab6f998b286e3058
ramondharmawan <ramondharmawan@gmail.com>
2022-03-15 14:17:32 +0800

    install yagmail for walletcreation

--- venlyapi/api/views.py
+++ venlyapi/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from . module.getToken import getTokens
+import smtplib, ssl
 
 
 # Create your views here.
@@ -63,11 +64,52 @@ def walletcreation(request):
 
     if request.method == 'POST':
         desc = request.POST['desc']
-        secrettype = request.POST['secrettype'],
-        wallettype = request.POST['wallettype'],
-        pincode = request.POST['pincode'],
+        username = request.POST['username']
+        email = request.POST['email']
+        secrettype = request.POST['secrettype']
+        wallettype = request.POST['wallettype']
+        pincode = request.POST['pincode']
     else:
         return render(request,'api/walletcreation.html')
 
+def deployNft(request):
+    token = getTokens(HttpResponse)
+
+    if request.method == 'POST':
+        aplid = request.POST['applicationId']
+        descnft = request.POST['descriptionNft']
+        image = request.POST['imageUrl']
+        chainNft = request.POST['chainNft']
+        ownerNft = request.POST['walletaddress']
+
+
+    url = "https://api-business-staging.arkane.network/api/apps/dummy-applicationid-abc123/contracts"
+
+    payload = json.dumps({
+    "name": "Venly",
+    "description": "Example contract created using the VENLY NFT API",
+    "image": "https://gblobscdn.gitbook.com/spaces%2F-MB9NX0aClLh4Gx_kawF%2Favatar-1623151896040.png?alt=media",
+    "externalUrl": "https://venly.io",
+    "media": [
+        {
+        "type": "twitter",
+        "value": "https://twitter.com/Venly_io"
+        },
+        {
+        "type": "linkedin",
+        "value": "https://www.linkedin.com/company/venly-io"
+        }
+    ],
+    "chain": "MATIC",
+    "owner": "0xF4Cc6E6c585d23585d5F08BDaEE9b85aB658fa95"
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization": "Bearer {}'.format(token)
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
 
 
